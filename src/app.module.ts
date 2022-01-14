242e41878769de1eb0411abe34ea859ff45af198
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-01-14 18:40:03 +0300

    Update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { PostController } from './post/post.controller';
+import { CommentController } from './comment/comment.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AuthController, PostController, CommentController],
   providers: [AppService],
 })
 export class AppModule {}
