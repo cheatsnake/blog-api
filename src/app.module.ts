b960a21b7c515c80eaf2024624812fe14deccdcc
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-01-15 19:36:26 +0300

    Connect environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
-import { PostController } from './post/post.controller';
-import { CommentController } from './comment/comment.controller';
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth/auth.controller";
+import { PostController } from "./post/post.controller";
+import { CommentController } from "./comment/comment.controller";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
-  controllers: [AppController, AuthController, PostController, CommentController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot()],
+    controllers: [AuthController, PostController, CommentController],
+    providers: [],
 })
 export class AppModule {}
