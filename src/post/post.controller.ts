79c9bfc63d8b8d9f0ac044f92d507d1946e5995e
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-01-16 19:00:41 +0300

    Update post controller

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -4,28 +4,54 @@ import {
     Delete,
     Get,
     HttpCode,
+    NotFoundException,
     Param,
     Patch,
     Post,
 } from "@nestjs/common";
 import { FindPostDto } from "./dto/find-post.dto";
+import { POST_NOT_FOUND } from "./post.constants";
 import { PostModel } from "./post.model";
+import { PostService } from "./post.service";
 
 @Controller("post")
 export class PostController {
+    constructor(private readonly postService: PostService) {}
+
     @Post("create")
-    async create(@Body() dto: PostModel) {}
+    async create(@Body() dto: PostModel) {
+        return this.postService.create(dto);
+    }
 
     @Get(":id")
-    async findById(@Param("id") id: string) {}
+    async findById(@Param("id") id: string) {
+        const post = await this.postService.findById(id);
+        if (!post) {
+            throw new NotFoundException(POST_NOT_FOUND);
+        }
+        return post;
+    }
 
     @Delete(":id")
-    async deleteById(@Param("id") id: string) {}
+    async deleteById(@Param("id") id: string) {
+        const deletedPost = await this.postService.deleteById(id);
+        if (!deletedPost) {
+            throw new NotFoundException(POST_NOT_FOUND);
+        }
+    }
 
     @Patch(":id")
-    async updateById(@Param("id") id: string, @Body() dto: PostModel) {}
+    async updateById(@Param("id") id: string, @Body() dto: PostModel) {
+        const updatedPost = await this.postService.updateById(id, dto);
+        if (!updatedPost) {
+            throw new NotFoundException(POST_NOT_FOUND);
+        }
+        return updatedPost;
+    }
 
     @HttpCode(200)
     @Post("find")
-    async findByCategory(@Body() dto: FindPostDto) {}
+    async findByCategory(@Body() dto: FindPostDto) {
+        return await this.postService.findByCategory(dto);
+    }
 }
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { TypegooseModule } from "nestjs-typegoose";
 import { PostController } from "./post.controller";
 import { PostModel } from "./post.model";
+import { PostService } from './post.service';
 
 @Module({
     controllers: [PostController],
@@ -15,6 +16,6 @@ import { PostModel } from "./post.model";
             },
         ]),
     ],
-    providers: [],
+    providers: [PostService],
 })
 export class PostModule {}
