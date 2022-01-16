b21f9c23fd2e8624c0ee1592036cd266277def0a
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-01-16 18:57:23 +0300

    Create post service

---
+++ src/post/post.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from "@nestjs/common";
+import { ModelType } from "@typegoose/typegoose/lib/types";
+import { InjectModel } from "nestjs-typegoose";
+import { CommentModel } from "src/comment/comment.model";
+import { CreatePostDto } from "./dto/create-post.dto";
+import { FindPostDto } from "./dto/find-post.dto";
+import { PostModel } from "./post.model";
+
+@Injectable()
+export class PostService {
+    constructor(
+        @InjectModel(PostModel)
+        private readonly postModel: ModelType<PostModel>
+    ) {}
+
+    async create(dto: CreatePostDto) {
+        return this.postModel.create(dto);
+    }
+
+    async findById(id: string) {
+        return await this.postModel.findById(id).exec();
+        // return this.postModel
+        //     .aggregate()
+        //     .match({ _id: new Types.ObjectId(id) })
+        //     .lookup({
+        //         from: "Comment",
+        //         localField: "_id",
+        //         foreignField: "postId",
+        //         as: "comments",
+        //     })
+        //     .exec() as unknown as PostModel &
+        //     {
+        //         comments: CommentModel[];
+        //     }[];
+    }
+
+    async deleteById(id: string) {
+        return await this.postModel.findByIdAndDelete(id).exec();
+    }
+
+    async updateById(id: string, dto: CreatePostDto) {
+        return this.postModel.findByIdAndUpdate(id, dto, { new: true }).exec();
+    }
+
+    async findByCategory(dto: FindPostDto) {
+        return this.postModel
+            .aggregate([
+                {
+                    $match: {
+                        category: dto.category,
+                    },
+                },
+                {
+                    $sort: {
+                        _id: 1,
+                    },
+                },
+                {
+                    $limit: dto.limit,
+                },
+                {
+                    $lookup: {
+                        from: "Comment",
+                        localField: "_id",
+                        foreignField: "postId",
+                        as: "comments",
+                    },
+                },
+            ])
+            .exec() as unknown as (PostModel & {
+            comments: CommentModel[];
+        })[];
+    }
+}
