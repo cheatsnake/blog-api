1e68b5ade01fe379571334dda7c755869f60db10
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-01-16 16:34:08 +0300

    Update post module

--- src/post/post.model.ts
+++ src/post/post.model.ts
@@ -1,7 +1,20 @@
-export class PostModel {
+import { prop } from "@typegoose/typegoose";
+import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+
+export interface PostModel extends Base {}
+export class PostModel extends TimeStamps {
+    @prop()
     category: string;
+
+    @prop()
     title: string;
+
+    @prop({ type: () => [String] })
     tags: string[];
+
+    @prop()
     image: string;
+
+    @prop()
     content: string;
 }
---
+++ src/post/post.module.ts
@@ -0,0 +1,20 @@
+import { Module } from "@nestjs/common";
+import { TypegooseModule } from "nestjs-typegoose";
+import { PostController } from "./post.controller";
+import { PostModel } from "./post.model";
+
+@Module({
+    controllers: [PostController],
+    imports: [
+        TypegooseModule.forFeature([
+            {
+                typegooseClass: PostModel,
+                schemaOptions: {
+                    collection: "Post",
+                },
+            },
+        ]),
+    ],
+    providers: [],
+})
+export class PostModule {}
