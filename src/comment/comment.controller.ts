9d7a1bc29127b89e181a2fc24c13bee7d65e9105
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-01-16 17:07:15 +0300

    Update comment controller

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -4,20 +4,35 @@ import {
     Delete,
     Get,
     HttpCode,
+    HttpException,
+    HttpStatus,
     Param,
     Patch,
     Post,
 } from "@nestjs/common";
-import { CommentModel } from "./comment.model";
+import { COMMENT_NOT_FOUND } from "./comment.constants";
+import { CommentService } from "./comment.service";
+import { CreateCommentDto } from "./dto/create-comment.dto";
 
 @Controller("comment")
 export class CommentController {
+    constructor(private readonly commentService: CommentService) {}
+
     @Post("create")
-    async create(@Body() dto: CommentModel) {}
+    async create(@Body() dto: CreateCommentDto) {
+        this.commentService.create(dto);
+    }
 
     @Get("byPost/:postId")
-    async findByPostId(@Param("postId") postId: string) {}
+    async findByPostId(@Param("postId") postId: string) {
+        return this.commentService.findByPostId(postId);
+    }
 
     @Delete(":id")
-    async deleteById(@Param("id") id: string) {}
+    async deleteById(@Param("id") id: string) {
+        const deletedComment = await this.commentService.deleteById(id);
+        if (!deletedComment) {
+            throw new HttpException(COMMENT_NOT_FOUND, HttpStatus.NOT_FOUND);
+        }
+    }
 }
