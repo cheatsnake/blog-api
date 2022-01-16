7cbe1722fd3d19ffdd3864630cdcdd6b5f02d42f
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-01-16 17:06:39 +0300

    Add comment service

---
+++ src/comment/comment.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import { ModelType, DocumentType } from "@typegoose/typegoose/lib/types";
+import { Types } from "mongoose";
+import { InjectModel } from "nestjs-typegoose";
+import { CommentModel } from "./comment.model";
+import { CreateCommentDto } from "./dto/create-comment.dto";
+
+@Injectable()
+export class CommentService {
+    constructor(
+        @InjectModel(CommentModel)
+        private readonly commentModel: ModelType<CommentModel>
+    ) {}
+
+    async create(dto: CreateCommentDto) {
+        return await this.commentModel.create(dto);
+    }
+
+    async findByPostId(postId: string): Promise<DocumentType<CommentModel>[]> {
+        return this.commentModel
+            .find({ postId: new Types.ObjectId(postId) })
+            .exec();
+    }
+
+    async deleteById(id: string): Promise<DocumentType<CommentModel> | null> {
+        return await this.commentModel.findByIdAndDelete(id).exec();
+    }
+}
