6a8b2ad98afe0636002cc0633f84bbc7449ca48d
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-01-17 16:44:05 +0300

    Create admin service

---
+++ src/auth/admin.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import { ModelType } from "@typegoose/typegoose/lib/types";
+import { genSalt, hash } from "bcryptjs";
+import { InjectModel } from "nestjs-typegoose";
+import { AdminModel } from "./admin.model";
+import { AuthDto } from "./dto/auth.dto";
+
+@Injectable()
+export class AdminService {
+    constructor(
+        @InjectModel(AdminModel)
+        private readonly adminModel: ModelType<AdminModel>
+    ) {}
+
+    async createAdmin(dto: AuthDto) {
+        const salt = await genSalt(10);
+        const newAdmin = new this.adminModel({
+            email: dto.email,
+            passwordHash: await hash(dto.password, salt),
+        });
+
+        return newAdmin.save();
+    }
+
+    async findAdmin(email: string) {
+        return this.adminModel.findOne({ email }).exec();
+    }
+}
